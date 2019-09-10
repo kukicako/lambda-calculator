682cb8701919ea125d94b43b02a6416d893aba29
kukicako <kukicako@gmail.com>
2019-09-10 15:35:22 -0700

    numbers page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import NumberButton from "./components/ButtonComponents/NumberButtons/Numbers"
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -17,7 +18,7 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <NumberButton />
       </div>
     </div>
   );
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { specials } from "../../../data";
 
 //import any components needed
 
