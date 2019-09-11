6a50053a7c11e0ca31e9dd6e77dbf410e1aaf3b6
kukicako <kukicako@gmail.com>
2019-09-10 18:34:14 -0700

    styling attempted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 import NumberButton from "./components/ButtonComponents/NumberButtons/Numbers";
 import SpecialButton from "./components/ButtonComponents/SpecialButtons/Specials";
-
+import OperatorButton from "./components/ButtonComponents/OperatorButtons/Operators";
+import Display from "./components/DisplayComponents/Display"
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -18,12 +19,20 @@ function App() {
 
   return (
     <div className="container">
-      <Logo />
+        <Logo />
       <div className="App">
+        <Display/>
+        </div>
+        <div className="numbers">
+      <div className="leftSide">
         <NumberButton />
         <SpecialButton/>
       </div>
-    </div>
+      <div className="rightSide">
+        <OperatorButton/>
+      </div>
+      </div>
+      </div>
   );
 }
 
